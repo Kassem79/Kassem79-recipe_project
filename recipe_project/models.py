a1157923460533596f25145aadacae12d6504cf4
Kassem79 <sherifkassem@hotmail.com>
2026-03-05 21:10:36 +0000

    Add base html and temolates

--- recipe_project/models.py
+++ recipe_project/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Recipe(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    ingredients = models.TextField()
+    instructions = models.TextField()
+    image = models.ImageField(upload_to='recipes/')
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
