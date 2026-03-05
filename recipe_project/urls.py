418f3ce122e5e16cb2a8c0572f0569be0f946200
Kassem79 <sherifkassem@hotmail.com>
2026-03-05 21:26:41 +0000

    create views and urls path

--- recipe_project/urls.py
+++ recipe_project/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
+from import views
 from .views import home_page_view
 
 
 urlpatterns = [
-    path("", home_page_view),
+    path('', views.home),
+    path('recipes/', views.recipe_list),
+    path('recipe/<int:id>/', views.recipe_detail),
 ]
 
 
--- recipe_project/views.py
+++ recipe_project/views.py
@@ -1,7 +1,19 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Recipe
 
 # Create your views here.
 
-def home_page_view(request):
-    return HttpResponse("<h1>Recipe, project</>")
+# def home_page_view(request):
+#    return HttpResponse("<h1>Recipe, project</>")
+def home(request):
+    recipes = Recipe.objects.all()[:6]
+    return render(request, 'home.html', {'recipes': recipes})
+
+def recipe_list(request):
+    recipes = Recipe.objects.all()
+    return render(request, 'recipes.html', {'recipes': recipes})
+
+def recipe_detail(request, id):
+    recipe = Recipe.objects.get(id=id)
+    return render(request, 'recipe_detail.html', {'recipe': recipe})
