bdb84bd1081bb55eb7ace0205ab1afc0e27dc33f
Kassem79 <sherifkassem@hotmail.com>
2026-03-07 14:51:03 +0000

    Updating home.html page and urls path link

--- recipe_project/urls.py
+++ recipe_project/urls.py
@@ -3,15 +3,12 @@ from . import views
 
 
 urlpatterns = [
-    path('', views.home),
-    path('recipes/', views.recipe_list),
-    path('recipe/<int:id>/', views.recipe_detail),
     path('', views.home, name='home'),
     path('recipes/', views.recipe_list, name='recipe_list'),
     path('recipe/<int:id>/', views.recipe_detail, name='recipe_detail'),
     path('login/', views.login_view, name='login'),
     path('logout/', views.logout_view, name='logout'),
-    path("categories/", views.categories, name="categories"),
+    path('categories/', views.categories, name='categories'),
 ]
 
 
--- recipe_project/views.py
+++ recipe_project/views.py
@@ -1,46 +1,53 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-from .models import Recipe
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
-from django.contrib.auth.decorators import login_required
-from .models import Category
+from .models import Recipe, Category
 
-
-# Create your views here.
-
-# def home_page_view(request):
-#    return HttpResponse("<h1>Recipe, project</>")
+# Home page view
 def home(request):
-    recipes = Recipe.objects.all()[:6]
-    return render(request, 'home.html', {'recipes': recipes})
+    recipes = Recipe.objects.all()
+    breakfast_recipes = recipes.filter(category__name='Breakfast')
+    lunch_recipes = recipes.filter(category__name='Lunch')
+    dinner_recipes = recipes.filter(category__name='Dinner')
+    dessert_recipes = recipes.filter(category__name='Desserts')
 
+    context = {
+        'recipes': recipes,
+        'breakfast_recipes': breakfast_recipes,
+        'lunch_recipes': lunch_recipes,
+        'dinner_recipes': dinner_recipes,
+        'dessert_recipes': dessert_recipes,
+    }
+    return render(request, 'home.html', context)
+
+# Recipe list view
 def recipe_list(request):
     recipes = Recipe.objects.all()
     return render(request, 'recipes.html', {'recipes': recipes})
 
+# Recipe detail view
 def recipe_detail(request, id):
     recipe = Recipe.objects.get(id=id)
     return render(request, 'recipe_detail.html', {'recipe': recipe})
 
+# Login view
 def login_view(request):
     if request.method == "POST":
         username = request.POST['username']
         password = request.POST['password']
         user = authenticate(request, username=username, password=password)
-        
         if user is not None:
             login(request, user)
-            return redirect('home')  # Redirect to home page after login
+            return redirect('home')
         else:
             return render(request, 'login.html', {'error': 'Invalid username or password'})
-    
     return render(request, 'login.html')
 
+# Logout view
 def logout_view(request):
     logout(request)
     return redirect('home')
 
+# Categories view
 def categories(request):
     categories = Category.objects.all()
     return render(request, "categories.html", {"categories": categories})
