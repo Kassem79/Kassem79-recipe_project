02efd934843f43ca7898a80a4531b8448da67d85
Kassem79 <sherifkassem@hotmail.com>
2026-03-18 11:38:46 +0000

    server error fixed

--- config/settings.py
+++ config/settings.py
@@ -32,7 +32,7 @@ if os.path.isfile("env.py"):
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = "DEVELOPMENT" in os.environ
+DEBUG = False
 
 ALLOWED_HOSTS = [
     '.herokuapp.com',
